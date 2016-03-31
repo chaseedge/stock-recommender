3f4939b29f326b565f19ec7aa8767620de1bc50b
Chase <csedge2@gmail.com>
2016-03-31 18:08:44 +0100

    Price-target-added
    Price target added, basic app works now.

--- scripts/stocks.js
+++ scripts/stocks.js
@@ -1,13 +1,12 @@
 /*Table of Contents
 1. Populate Autocomplete Fields
 2. Pulling in Data
-3. Calculate Information
-4. Output Data
-
-
-
+3. Parsing Data
+4. Calculting Target Price
+5. HTML Output
 */
-// global variables
+
+
 //1. Populate Autocomplete Fields
 // Uses "Awesomplete" and an object of all tickers
 //
@@ -41,19 +40,11 @@ function addTickers() {
 
 
 //2. Pulling in Data
-//var link = "https://www.quandl.com/api/v3/datasets/SF0/"+ ticker +"_EBITDA_MRY.json?auth_token=NqfzA-DNoQiwhzYiqZCi";
-//var link = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22AAPL%22%29&format=json&env=https://raw.githubusercontent.com/cynwoody/yql-tables/finance-1/tables.env";
-// var statsLink = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.keystats%20where%20symbol%20in%20("YHOO","AAPL","GOOG","MSFT")&format=json&env=https://raw.githubusercontent.com/cynwoody/yql-tables/finance-1/tables.env";
-// var quoteLink = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22AAPL%22)&format=json&env=https://raw.githubusercontent.com/cynwoody/yql-tables/finance-1/tables.env";
-// var peersLink = "http://api.capitalcube.com/companies/" + ticker + "-us/peers";
-
-
 
 function getData() {
 	var ticker = document.getElementById("ticker").value;
 	var targetPrices = {};
 	var compInfo = {};
-	var priceTarget = {};
 	var multiples = {
 		priceEarnings : {},
 		priceBook : {},
@@ -61,32 +52,23 @@ function getData() {
 		evFcf : {}
 	};
 
-
-	var colsTopTable = ["labels", "rating", "empty", "peerMin", "peerMedian", "peerMax"];
-	var propArr = ["LastTradePriceOnly","EarningsShare","EBITDA","EnterpriseValue",
-	"BookValuePerShare","symbol","TotalDebt","TotalCash","Name","SharesOutstanding"];
-
-
 	function quote(data){
 		data = data.query.results.quote;
-		compInfo["textEBITDA"] = data["EBITDA"];
-		addProp(data, compInfo, propArr);
-		console.log(compInfo);
+		parseQuote(data, compInfo);
+		console.log("finished with quote data");
 	}
 
 	function stats(data){
 		data = data.query.results.stats;
-		console.log(data);
-		addProp(data, compInfo, propArr);
-		console.log(compInfo);
+		parseStats(data, compInfo);
+		console.log("finished with stats data");
 	}
 
 	function peers(data){
 		data = data.keyValuationItems;
 		parsePeers(data, multiples, compInfo);
-		htmlMultiples(multiples, colsTopTable);
-		htmlCompInfo(compInfo);
-		console.log(multiples);
+		htmlMultiples(multiples);
+		console.log("finished with peers data");
 	}
 
 	infoRequest(buildLink(ticker, "quote")).then(function(data){
@@ -95,12 +77,36 @@ function getData() {
 			stats(data);
 			return infoRequest(buildLink(ticker, "peers")).then(function(data){
 				peers(data);
-				calcTargetPrice(compInfo, multiples, priceTarget);
+				equityTargetPrice(compInfo, multiples, targetPrices);
+				evEbitdaTargetPrice(compInfo, multiples, targetPrices);
+				evFcfTargetPrice(compInfo, multiples, targetPrices);
+				averagePrices(targetPrices);
+				htmlTest([compInfo.price],[1], "price1");
+				htmlTest([compInfo.price],[5], "price2");
+				htmlTest([compInfo.eps, multiples.pe.peerMedian, targetPrices.pe],[1, 4, 5], "peTarget");
+				htmlTest([compInfo.bvps, multiples.pb.peerMedian, targetPrices.pb],[1, 4, 5], "pbTarget");
+				htmlTest([compInfo.ebitdaText, multiples.evEbitda.peerMedian, targetPrices.evEbitda],[1, 4, 5], "evEbitdaTarget");
+				htmlTest([compInfo.fcfText, multiples.evFcf.peerMedian, targetPrices.evFcf],[1, 4, 5], "evFcfTarget");
+				htmlTest([targetPrices.average],[5], "average");
+				commentary(compInfo, targetPrices);
 			})
 		})
 	}).catch(function(error) {console.log("ERROR - SOMETHING WENT TERRIBLY WRONG!");});
 
 
+}
+
+//2. Pulling data
+//functions that pull data in from two sources, yahoo and capitalCube
+function buildLink(ticker, database){
+	var link;
+	if (database === "stats")
+		link = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.keystats%20where%20symbol%20in%20('" + ticker + "')&format=json&env=https://raw.githubusercontent.com/cynwoody/yql-tables/finance-1/tables.env";
+	if (database === "quote")
+		link = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20('" + ticker + "')&format=json&env=https://raw.githubusercontent.com/cynwoody/yql-tables/finance-1/tables.env";
+	if (database === "peers")
+		link = "http://api.capitalcube.com/companies/" + ticker + "-us/reports/peer-characteristics";
+	return link;
 }
 
 function infoRequest (url) {
@@ -121,59 +127,116 @@ function infoRequest (url) {
   });
 }
 
-function parseQuote(data){}
+//3. Parsing Data
+// Takes the data and populates the appropriate objects
+
+//some data is returned in text like 640.23B, this converts it to a digit
+function textToNum(num){
+  var len = num.length - 1;
+  var unit = num.charAt(len).toUpperCase();
+  var newNum = Number(num.slice(0, len));
+  if(unit === "B")
+    newNum = newNum * 1000000000;
+  if(unit === "M")
+    newNum = newNum * 1000000;
+  return newNum;
+}
+
+//converts numbers into text with units  12343334 -> 12.34M
+function numToText(num){
+	var len = num.length;
+	var textNum;
+	if (len > 9) {
+		textNum = (num / 1000000000).toFixed(2);
+		textNum += "B";
+	} else {
+		textNum = (num / 1000000).toFixed(2);
+		textNum += "M";
+	}
+	return textNum;
+}
+
+function parseQuote(data, compInfo){
+	var apiLabels = ["BookValue", "EBITDA", "EarningsShare", "LastTradePriceOnly", "MarketCapitalization", "Name", "OneyrTargetPrice", "symbol" ];
+	var newLabels = ["bvps", "ebitdaText", "eps", "price", "mktCapText", "name", "yahooTargetPrice", "symbol"];
+	for (var i = 0; i < apiLabels.length; i++) {
+		compInfo[newLabels[i]] = data[apiLabels[i]];
+	}
+	compInfo["mktCap"] = textToNum(compInfo["mktCapText"]);
+}
+
+function parseStats(data, compInfo){
+	compInfo["ev"] = data["EnterpriseValue"]["content"];
+	compInfo["ebitda"] = data["EBITDA"]["content"];
+	compInfo["shares"] = data["SharesOutstanding"];
+	compInfo["netDebt"] = compInfo["ev"] - compInfo["mktCap"];
+}
 
 function parsePeers(data, multiples, compInfo){
-	multiples["priceEarnings"] = data["Price to Earnings (P/E)"];
-	multiples["priceBook"] = data["Price to Book (P/B)"];
+	multiples["pe"] = data["Price to Earnings (P/E)"];
+	multiples["pb"] = data["Price to Book (P/B)"];
 	multiples["evEbitda"] = data["EV / EBITDA"];
 	multiples["evFcf"] = data["EV / FCF"];
-	compInfo["fcf"] = Number(compInfo["EnterpriseValue"]) / Number(multiples["evFcf"]["rating"]);
+	compInfo["fcf"] = Number(compInfo["ev"]) / Number(multiples["evFcf"]["rating"]);
 	compInfo["fcf"] = compInfo["fcf"].toFixed(0);
+	compInfo.fcfText = numToText(compInfo.fcf);
 }
-function calcTargetPrice(compInfo, multiples, targetPrices) {
-	var pe = multiples.priceEarnings.peerMedian;
-	var eps = compInfo.EarningsShare;
-	var pb = multiples.priceBook.peerMedian;
-	var bv = compInfo.BookValuePerShare;
-	if (!isNaN(pe * eps)) {
-		targetPrices["PE"] = pe * eps;
+
+
+
+function equityTargetPrice(compInfo, multiples, targetPrices) {
+	var pe = multiples.pe.peerMedian;
+	var eps = compInfo.eps;
+	var pb = multiples.pb.peerMedian;
+	var bvps = compInfo.bvps;
+	if (!isNaN(pe * eps) && eps > 0) {
+		targetPrices.pe = pe * eps;
+	} else {
+		targetPrices.pe = "n/a";
 	}
-	if (!isNaN(pb * bv)) {
-		targetPrices["PB"] = pb * bv;
+	if (!isNaN(pb * bvps) && bvps > 0) {
+		targetPrices.pb = pb * bvps;
+	} else {
+		targetPrices.pb = "n/a";
 	}
-	console.log(targetPrices);
+}
+
+function evEbitdaTargetPrice(compInfo, multiples, targetPrices) {
+	var ebitdaMultiple = multiples.evEbitda.peerMedian;
+	var ebitda = compInfo.ebitda;
+	var shares = Number(compInfo.shares);
+	if (!isNaN(ebitdaMultiple * ebitda) && ebitda > 0) {
+		var evEbitda = ebitdaMultiple * ebitda;
+		var mktCap = evEbitda - compInfo.netDebt;
+		targetPrices.evEbitda = (mktCap / shares);
+	} else {
+		targetPrices.evEbitda = "n/a";
+	}
+}
+
+function evFcfTargetPrice(compInfo, multiples, targetPrices) {
+	var fcfMultiple = multiples.evFcf.peerMedian;
+	var fcf = compInfo.fcf;
+	var shares = Number(compInfo.shares);
+	if (!isNaN(fcfMultiple * fcf)) {
+		var evFcf = fcfMultiple * fcf;
+		var mktCap = evFcf - compInfo.netDebt;
+		if (mktCap > 0) {
+				targetPrices.evFcf = (mktCap / shares);
+		} else {
+				targetPrices.evFcf = "n/a";
+		}
+	} else {
+		targetPrices.evFcf = "n/a";
+	}
 }
 
 
-
-
-function buildLink(ticker, database){
-	var link;
-	if (database === "stats")
-		link = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.keystats%20where%20symbol%20in%20('" + ticker + "')&format=json&env=https://raw.githubusercontent.com/cynwoody/yql-tables/finance-1/tables.env";
-	if (database === "quote")
-		link = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20('" + ticker + "')&format=json&env=https://raw.githubusercontent.com/cynwoody/yql-tables/finance-1/tables.env";
-	if (database === "peers")
-		link = "http://api.capitalcube.com/companies/" + ticker + "-us/reports/peer-characteristics";
-	return link;
-}
-
-function addProp(data, targetObj, propArr) {
-  for (var i = 0; i < propArr.length; i++){
-    var property = propArr[i];
-    if (data.hasOwnProperty(property)) {
-        targetObj[property] = data[property];
-        if(targetObj[property].hasOwnProperty("content"))
-          	targetObj[property] = targetObj[property]["content"];
-      }
-   }
-}
-
-function htmlMultiples(obj, columns) {
+function htmlMultiples(obj) {
+	var columnLabels = ["labels", "rating", "empty", "peerMin", "peerMedian", "peerMax"];
 	for (var x in obj) {
 		for (var key in obj[x]) {
-			var col = columns.indexOf(key);
+			var col = columnLabels.indexOf(key);
 			var value = Number(obj[x][key]).toFixed(2);
 			if (isNaN(value)){
 				value = "n/a";
@@ -193,20 +256,53 @@ function formatNum(num) {
   });
 }
 
-function htmlCompInfo(compInfo){
-	var row = document.getElementById("LastTradePriceOnly");
+function htmlTest(dataArr,colArr, htmlId) {
+	var row = document.getElementById(htmlId);
 	var cells = row.getElementsByTagName("td");
-	cells[1].innerHTML = "$" + compInfo["LastTradePriceOnly"];
-	row = document.getElementById("priceEarningsTarget");
-	var cells = row.getElementsByTagName("td");
-	cells[1].innerHTML = "$" + compInfo["EarningsShare"];
-	row = document.getElementById("priceBookTarget");
-	var cells = row.getElementsByTagName("td");
-	cells[1].innerHTML = "$" + compInfo["BookValuePerShare"];
-	row = document.getElementById("evEbitdaTarget");
-	var cells = row.getElementsByTagName("td");
-	cells[1].innerHTML = "$" + compInfo["textEBITDA"];
-	row = document.getElementById("evFcfTarget");
-	var cells = row.getElementsByTagName("td");
-	cells[1].innerHTML = formatNum(compInfo["fcf"]);
+	for (var i = 0; i < dataArr.length; i++) {
+		var col = colArr[i];
+		var value = dataArr[i];
+		if (!isNaN(value)) {
+			value = Number(value).toFixed(2);
+		}
+		cells[col].innerHTML = value;
+	}
+}
+
+function averagePrices(targetPrices){
+	var sum = 0;
+	var count = 0;
+	var average;
+	for (var key in targetPrices) {
+		if (!isNaN(targetPrices[key])) {
+			sum += targetPrices[key];
+			count++
+		}
+	}
+	average = sum / count;
+	if (!isNaN(average)) {
+		targetPrices.average = average.toFixed(2);
+	} else {
+		targetPrices.average = "n/a";
+	}
+}
+
+function commentary(compInfo, targetPrices) {
+	var name = compInfo.name;
+	var price = compInfo.price;
+	var calcPrice = targetPrices.average;
+	var diff = calcPrice - price;
+	var text;
+	if (isNaN(diff)) {
+		text = "Sorry, there was not enough data points for " + name + " to make a recommendation. Please try a different ticker.";
+	}
+	if (diff > 0) {
+		text = "Based on these metrics, it appears that " + name + " is undervalued by $" + diff.toFixed(2) + ". So it might be a good time to buy!";
+	} else  if (diff < 0){
+		text = "Based on these metrics, it appears that " + name + " is overvalued by $" + diff.toFixed(2) + ". So you might want to wait to buy any shares or maybe short it!";
+	} else if (diff = 0) {
+		text = name + " appears to be valued appropriately!";
+	}
+	var div = document.getElementById("commentary");
+	div.innerHTML = text;
 }
